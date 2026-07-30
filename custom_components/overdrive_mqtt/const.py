d438b62802c6532c7f4d4de0a3f2fe3b3b30f2f9
Mora Jimmy <72866243+jimbonk2000@users.noreply.github.com>
2026-07-31 01:19:22 +0700

    Update const.py

--- custom_components/overdrive_mqtt/const.py
+++ custom_components/overdrive_mqtt/const.py
@@ -1,2 +1,17 @@
 DOMAIN = "overdrive_mqtt"
-DEFAULT_TOPIC = "overdrive/status"
+DEFAULT_NAME = "Overdrive Vehicle Integration"
+DEFAULT_TELEMETRY_TOPIC = "overdrive/vehicle/telemetry"
+DEFAULT_AVAILABILITY_TOPIC = "overdrive/vehicle/telemetry/availability"
+
+CONF_TELEMETRY_TOPIC = "telemetry_topic"
+CONF_AVAILABILITY_TOPIC = "availability_topic"
+
+PLATFORMS = ["sensor", "binary_sensor", "device_tracker"]
+
+INVALID_VALUES = {
+    65535,
+    1048575,
+    104857.5,
+    -10011,
+    -2147482648,
+}
