7ebd612fa653ae7bdd5b5b955ae51b92cf699168
Mora Jimmy <72866243+jimbonk2000@users.noreply.github.com>
2026-07-31 01:47:32 +0700

    Update config_flow.py

--- custom_components/overdrive_mqtt/config_flow.py
+++ custom_components/overdrive_mqtt/config_flow.py
@@ -2,7 +2,6 @@ from homeassistant import config_entries
 import voluptuous as vol
 from .const import (
     DOMAIN, 
-    DEFAULT_NAME,
     CONF_TELEMETRY_TOPIC, 
     CONF_AVAILABILITY_TOPIC, 
     DEFAULT_TELEMETRY_TOPIC, 
@@ -14,10 +13,15 @@ class OverdriveConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title=DEFAULT_NAME, data=user_input)
+            return self.async_create_entry(
+                title=user_input.get("name", "Overdrive Vehicle"), 
+                data=user_input
+            )
 
         schema = vol.Schema({
+            vol.Required("name", default="Overdrive Vehicle"): str,
             vol.Required(CONF_TELEMETRY_TOPIC, default=DEFAULT_TELEMETRY_TOPIC): str,
             vol.Required(CONF_AVAILABILITY_TOPIC, default=DEFAULT_AVAILABILITY_TOPIC): str,
         })
         return self.async_show_form(step_id="user", data_schema=schema)
+        
